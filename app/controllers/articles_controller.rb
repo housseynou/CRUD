0ec65f18af8f76d752da948ba79cdcf53f1faae6
housseynou coulibaly <housseynou7@gmail.com>
2021-07-13 21:46:45 +0200

    update un article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
   def show
     @article = Article.find(params[:id])
   end
+  
   def new
     @article = Article.new
   end
@@ -18,4 +20,23 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
